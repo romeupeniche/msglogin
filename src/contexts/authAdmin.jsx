73002bafea04c689dc01bd88ff04520eb0fd9123
romeupeniche <romeupeniche12@gmail.com>
2023-03-25 00:43:11 -0300

    incompleted auth

---
+++ src/contexts/authAdmin.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export const AuthAdminProvider = ({ children }) => {
+    const [user, setUser] = useState('');
+
+
+    const signIn = () => {
+        s
+    }
+}
